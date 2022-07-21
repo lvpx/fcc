e42742db05815590b0fee6ff9408475c61f4127c
Lovepreet Singh <lovepreetkaul23@gmail.com>
2022-07-21 10:37:53 +0000

    Run npm tests

---
+++ server.test.js
@@ -0,0 +1,17 @@
+const server = require('./server')
+
+test('Performance Test #1', () => {
+    expect(true).toBe(true);
+});
+
+test('Unit Test #1', () => {
+    expect(true).toBe(true);
+});
+
+test('Performance Test #2', () => {
+    expect(true).toBe(true);
+});
+
+test('Unit Test #2', () => {
+    expect(true).toBe(false);
+});
